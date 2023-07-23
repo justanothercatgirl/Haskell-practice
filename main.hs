b7fc203bf688ea08f9c5d1ca62283aa4f741c640
Just Another Catgirl <73675324+justanothercatgirl@users.noreply.github.com>
2023-07-23 16:19:13 +0300

    Update main.hs

--- main.hs
+++ main.hs
@@ -14,18 +14,17 @@ prod xss =
     then [a:b | a <- head xss, b <- prod (tail xss)] 
     else prod2 xss
 
-f :: Bool a => a -> a -> a -> a -> a
 f x y z w = (x <= (z==w)) || not (y <= w)
 
 allTables = [ [[x1, True, x2, x3, False], 
                [False, x4, False, x5, False], 
                [x6, False, False, x7, False]] | [x1, x2, x3, x4, x5, x6, x7] <- prodRepeat [False, True] 7 ]
 
-fWrapper xsss = [[x1, x2, x3, x4, x5] | [[[x1, x2, x3, x4, x5]]] <- allTables, f x1 x2 x3 x4 == x5]
+fWrapper xsss = [[x1, x2, x3, x4, x5] | [[x1, x2, x3, x4, x5]] <- xsss, f x1 x2 x3 x4 == x5]
 
 main :: IO ()
 main = do
-print(test)
+print(fWrapper (allTables))
 
 
 {-
@@ -33,5 +32,3 @@ print(test)
     print( [x*y | x <- [1..4], y <- [2, 4..8], x*y >= 10] )
     https://www.kompege.ru/task
 -}
-
-
