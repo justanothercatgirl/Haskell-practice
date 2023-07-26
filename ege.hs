6696859ea556db8ea90e0199f4a62d6538aef295
Just Another Catgirl <73675324+justanothercatgirl@users.noreply.github.com>
2023-07-26 13:49:51 +0300

    new tasks added

--- ege.hs
+++ ege.hs
@@ -1,23 +1,51 @@
-prodRepeat xs 1 = [[a] | a <- xs]
-prodRepeat xs y = [a:b | a <- xs, b <- prodRepeat xs (y-1)]
+import Data.List
 
-count x [] = 0
-count x xs = if x == head xs then 1 + count x (tail xs) else count x (tail xs)
+path :: Int -> Int -> Int
+path start end  
+    | (start < end)      = 0
+    | (start == 7)       = 0
+    | (start == end)     = 1 
+    | otherwise          = path (start-1) end + path (start-3) end + path (div start 2) end
+    
+isqrt :: Int -> Int
+isqrt = floor . sqrt . fromIntegral
 
-count' :: [[Char]] -> Integer -> Integer
-count' xs accum = 
-    if (all [count c (head xs) == 1 | c <- head xs] && )
-    then accum
-    else count' (tail xs) (accum + 1)
+allDivisors :: Int -> [Int]
+allDivisors x = 
+    concat [if d1 /= d2 then [d1, d2] else [d1]
+        | d1 <- [1..n], 
+        let d2 = div x d1, 
+        mod x d1 == 0 ]
+    where n = isqrt x + 1
 
-lst = "0123456789ABCDEF"
+has_n_divisors :: Int -> Int -> Bool
+has_n_divisors x n = length (allDivisors x) == n
 
-digToDec 'x' = 
-digToDec x = (fromEnum x - fromEnum '0')
+tryNumbersRange :: (Int, Int) -> Int -> [[Int]]
+tryNumbersRange (low, upp) n = 
+    [take 2 ( sortBy (flip compare) (allDivisors x) ) 
+    | x <- [low..upp], has_n_divisors x n]
 
-toDec :: [Char] -> Int -> Integer
-toDec num base = 
+numbersMasked :: [Int]
+numbersMasked = [123450708 + x + y | x <- [0, 1000..9999], y <- [1, 10..99]]
+
+doDivisions :: Int -> [Int] -> [(Int, Int)]
+doDivisions n list = [(x, div x n) | x <- list, mod x n == 0]
+
+intToList :: Int -> [Int]
+intToList x = [fromEnum y - 48 | y <- show x]
+
+sumOddDigits :: Int -> Int
+sumOddDigits x = sum [d | d <- (intToList x), odd d]
+
+numMask1 = [12400579 + x | x <- [1000, 2000..99999]]
+numMask2 = [12450079 + x | x <- [100, 200..9999]]
+numMask3 = [12305079 + x + y | x <- [100, 200..999], y <- [10000, 20000..99999]]
+allNumsMasked = numMask1 ++ numMask2 ++ numMask3
+
+specificSort (a, _) (b, _) = compare a b
+
+doMaskings xs = [(x, sum . intToList $ x) | x <- xs, mod x (sumOddDigits x) == 0]
 
 main :: IO()
-main = do
-    print (9^5 + 1 - count' (reverse (prodRepeat lst 5)) 1) 
+main = print(sortBy specificSort . doMaskings $ allNumsMasked)
